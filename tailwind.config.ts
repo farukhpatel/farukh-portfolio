ef25939b4d17cc1f40ee299c4a090c3e1a214f4d
Farukh Patel <farukhpatel0804@gmail.com>
2024-09-29 11:35:08 +0530

    font setup and meta data updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        headings: ["Oswald", "sans-serif"],
+        para: ["Fredoka", "sans-serif"],
+      },
     },
   },
   plugins: [],
