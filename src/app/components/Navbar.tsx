63fd1f330d66329b31b9ae6d72f68511ce1fedc2
Farukh Patel <farukhpatel0804@gmail.com>
2024-10-07 19:18:03 +0530

    minor icon changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { navbarOptions } from "../utills/constant";
 import Image from "next/image";
 import icon from "../public/images/code.svg";
+import Menu from "../public/images/svgs/Menu";
 
 interface NavbarProps {
   name: string;
@@ -45,20 +46,7 @@ const Navbar: React.FC<NavbarProps> = ({ name }) => {
           onClick={toggleDropdown}
           className="text-white focus:outline-none"
         >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          <Menu/>
         </button>
 
         {/* Dropdown Content for Mobile */}
